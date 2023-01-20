9398814c084bbcbd728a801f63ad94aaacc7b37b
Taahaa <58344496+JJosuke@users.noreply.github.com>
2023-01-20 16:25:55 +0000

    grid started - configure cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -7,53 +7,24 @@ class App extends Component {
     super();
 
     this.state = {
-      pokemon: [
-        {
-          name: "Bulbasaur",
-          id: "pk1"
-        },
-        {
-          name: "Ivysaur",
-          id: "pk2"
-        },
-        {
-          name: "Venusaur",
-          id: "pk3"
-        },
-        {
-          name: "Squirtle",
-          id: "pk4"
-        },
-        {
-          name: "Wartortle",
-          id: "pk5"
-        },
-        {
-          name: "Blastoise",
-          id: "pk6"
-        },
-        {
-          name: "Charmander",
-          id: "pk7"
-        },
-        {
-          name: "Charmeleon",
-          id: "pk8"
-        },
-        {
-          name: "Charizard",
-          id: "pk9"
-        }
-      ]
+      pokemon: []
     };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pokemon")
+    .then(response => response.json())
+    .then(pokemon => this.setState({ pokemon : pokemon }))
   }
   
   render() {
     return(
       <div className="App">
+        <CardList name="Taahaa">
         {
           this.state.pokemon.map(pokemon => <h1 key={pokemon.id}>{pokemon.name }</h1>)
         }
+        </CardList>
       </div> 
     )
   }
