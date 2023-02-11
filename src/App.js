ef5be9f75dc5b882f0b0233a9a263eb53ab7084c
JJosuke <projectyuni5@gmail.com>
2023-02-11 01:55:40 +0000

    search box update

--- src/App.js
+++ src/App.js
@@ -21,7 +21,10 @@ class App extends Component {
   render() {
     return(
       <div className="App">
-        <input type="search" placeholder='Search Kanto Dex'/>
+        <input
+          type="search" placeholder='Search Kanto Dex'
+          onChange={e =>
+            this.setState({ searchField: e.target.value })}/>
         <CardList pokemon = {this.state.pokemon} />
       </div> 
     )
